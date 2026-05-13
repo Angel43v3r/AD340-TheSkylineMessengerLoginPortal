6dd36ae3af3b2d7ad6647a804c0a1cd3e9468590
Angel43ver <jovyannnelson@gmail.com>
2026-05-12 20:38:39 -0700

    Login Portal Screen

--- app/src/main/java/com/example/theskylinemessenger/MainActivity.kt
+++ app/src/main/java/com/example/theskylinemessenger/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.theskylinemessenger
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val signInButton = findViewById<MaterialButton>(R.id.signInButton)
+        val statusText = findViewById<TextView>(R.id.statusText)
+
+        // Click sign in
+        signInButton.setOnClickListener {
+            statusText.text = getString(R.string.signed_in)
         }
     }
 }
